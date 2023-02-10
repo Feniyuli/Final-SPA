e488d2b8ae7aaee6726b6303b68582f92e6debe9
Jörn Schimmelpfeng <joern.schimmelpfeng@gmail.com>
2023-02-10 21:04:57 +0100

    project init

---
+++ src/main/java/de/dhbw/dixit/DixitApplication.java
@@ -0,0 +1,13 @@
+package de.dhbw.dixit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DixitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DixitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/dhbw/dixit/DixitApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dhbw.dixit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DixitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
