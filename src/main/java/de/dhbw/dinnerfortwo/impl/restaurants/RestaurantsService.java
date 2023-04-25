167f81cd36dfbaf88da17776537f8ee4b3302f84
Feniyuli <Feni_yuli_a@yahoo.com>
2023-04-25 04:39:06 +0200

    Delete Restaurant

--- src/main/java/de/dhbw/dinnerfortwo/impl/restaurants/RestaurantsService.java
+++ src/main/java/de/dhbw/dinnerfortwo/impl/restaurants/RestaurantsService.java
@@ -1,7 +1,9 @@
 package de.dhbw.dinnerfortwo.impl.restaurants;
 
-import de.dhbw.dinnerfortwo.impl.reservation.Reservation;
-import de.dhbw.dinnerfortwo.impl.reservation.ReservationTO;
+import de.dhbw.dinnerfortwo.impl.item.Items;
+import de.dhbw.dinnerfortwo.impl.item.ItemsRepository;
+import de.dhbw.dinnerfortwo.impl.table.Tables;
+import de.dhbw.dinnerfortwo.impl.table.TablesRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -16,11 +18,15 @@ import java.util.stream.Collectors;
  */
 @Service
 public class RestaurantsService {
-    public RestaurantsService(RestaurantsRepository restaurantsRepository) {
+    public RestaurantsService(RestaurantsRepository restaurantsRepository, TablesRepository tablesRepository, ItemsRepository itemsRepository) {
         this.restaurantsRepository = restaurantsRepository;
+        this.tablesRepository = tablesRepository;
+        this.itemsRepository = itemsRepository;
     }
 
     private final RestaurantsRepository restaurantsRepository;
+    private final TablesRepository tablesRepository;
+    private final ItemsRepository itemsRepository;
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Transactional
@@ -66,6 +72,27 @@ public class RestaurantsService {
     @Transactional
     public void delete(Long id) {
         log.info("Deleting rating with id {}", id);
+
+        // Get the restaurant entity by ID
+        Restaurants restaurant = restaurantsRepository.findById(id).orElse(null);
+
+        // Check if restaurant exists
+        if (restaurant != null) {
+            // Get all tables in the restaurant
+            List<Tables> tables = tablesRepository.findAllTablesByRestaurantId(id);
+
+            // Delete all tables
+            tablesRepository.deleteAll(tables);
+
+            List<Items> items = itemsRepository.findAllItemsByRestaurantId(id);
+
+            // Delete all items
+            itemsRepository.deleteAll(items);
+        } else {
+            // Handle case where restaurant does not exist
+            throw new IllegalArgumentException("Restaurant with ID " + id + " does not exist");
+        }
+
         restaurantsRepository.deleteById(id);
     }
 
